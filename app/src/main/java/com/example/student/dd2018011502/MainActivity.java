b221528b6c8d7245ae480a21936a5cd0bd216c9e
kcli2412 <kcli2412@gmail.com>
2018-01-15 11:47:34 +0800

    Bitmap

--- app/src/main/java/com/example/student/dd2018011502/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018011502/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.student.dd2018011502;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void click1(View v)
+    {
+        new Thread()
+        {
+            @Override
+            public void run() {
+                super.run();
+
+                String str_url = "https://5.imimg.com/data5/UH/ND/MY-4431270/red-rose-flower-500x500.jpg";
+                URL url = null;
+                try {
+                    url = new URL(str_url);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    InputStream inputStream = conn.getInputStream();
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    byte[] buf = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(buf)) != -1)
+                    {
+                        bos.write(buf, 0, length);
+                    }
+
+                    byte[] results = bos.toByteArray();
+                    final Bitmap bmp = BitmapFactory.decodeByteArray(results, 0, results.length);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            img.setImageBitmap(bmp);
+                        }
+                    });
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
